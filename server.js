31b4166e39fc9c3c6b905d70fb1f455cf5518107
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-08-03 17:57:53 +0300

    connecting database

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require("mongodb");
+require("dotenv").config()
+const databaseURI = process.env.MONGO_URI
+
+let db,
+    dbName = "tasks"
+
+MongoClient.connect(databaseURI, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to the ${dbName} database`)
+        db = client.db(dbName)
+    })
+    .catch(err => {
+        console.error(err)  
+    })
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
